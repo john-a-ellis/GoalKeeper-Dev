bedf4e759d6973bc873dc330caa016049d604357
john-a-ellis <145600714+john-a-ellis@users.noreply.github.com>
2024-10-20 12:20:29 -0400

    moving embedder to api from local

---
+++ assets/api_keys.py
@@ -0,0 +1,14 @@
+# OpenWeatherMap API Key
+weather_api_key = "f2ec46818cbe3000b1ceda6237b665ac"
+
+# Geoapify API Key
+geoapify_key = "7925bd17d2c7459d81af8b05e56ce1f7"
+
+# HuggingFace API Key
+huggingface_key = "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"
+
+#Groq Cloud API Key
+groq_key ="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"
+
+#Alpha Vantage API Key
+alpha_vantage = 'TDL71VB2OQUAFGOW'
