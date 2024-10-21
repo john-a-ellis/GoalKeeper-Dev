e92fa3aa51c80a406919a6e5cbc281ae4a95130e
john-a-ellis <145600714+john-a-ellis@users.noreply.github.com>
2024-10-21 19:38:17 -0400

    fixing Auth2

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ from requests import request
 from dotenv import load_dotenv, find_dotenv
 import os
 
+is_deployed = os.getenv('DEPLOYED')
+
 #Load .env variables if not deployed.
-if not os.getenv('DEPLOYED'):
+if not is_deployed:
     load_dotenv(find_dotenv(raise_error_if_not_found=True))
     get_login =""
 else:
@@ -20,11 +22,10 @@ else:
     authorization_base_url = 'https://accounts.google.com/o/oauth2/auth'
     token_url = 'https://accounts.google.com/o/oauth2/token'
     scope = ["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"]
-
     get_login = dbc.Modal([
         dbc.Button("Login with Google", id="login-button", color="success"),
         dcc.Location(id='url', refresh=True),
-        html.Div(id='content')
+        html.Div(id='login-content')
     ], is_open = True)
 
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SKETCHY,
@@ -37,35 +38,38 @@ app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.SKETC
                                                                 
 server = app.server
 
-app.layout = ([
-        dbc.Modal([
-        dbc.Button("Login with Google", id="login-button", color="success"),
-        dcc.Location(id='url', refresh=True),
-        html.Div(id='content')
-    ], is_open = True),
-])
+app.layout = ([get_login,
+               dash.page_container]    
+            )
 
-dash.page_container
 
 @callback(Output('url', 'href'), [Input('login-button', 'n_clicks')])
 def login_with_google(n_clicks):
     if n_clicks:
-        google = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
-        authorization_url, state = google.authorization_url(authorization_base_url, access_type="offline")
-        return authorization_url
+        if is_deployed:
+            google = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
+            authorization_url, state = google.authorization_url(authorization_base_url, access_type="offline")
+            return authorization_url
+        
     return None
 
-@callback(Output('content', 'children'), [Input('url', 'search')])
+@callback(Output('login-content', 'children'), [Input('url', 'search')])
 def display_user_info(query_string):
-    if query_string:
-        google = OAuth2Session(client_id, redirect_uri=redirect_uri)
-        token = google.fetch_token(token_url, client_secret=client_secret, authorization_response=request.url)
-        user_info = google.get('https://www.googleapis.com/oauth2/v1/userinfo').json()
-        return html.Div([
-            html.H1(f"Welcome, {user_info['name']}"),
-            html.P(f"Email: {user_info['email']}")
-        ])
-    return "Please login with Google."
+    if is_deployed:
+        if query_string:
+            google = OAuth2Session(client_id, redirect_uri=redirect_uri)
+            token = google.fetch_token(token_url, client_secret=client_secret, authorization_response=request.url)
+            user_info = google.get('https://www.googleapis.com/oauth2/v1/userinfo').json()
+            return html.Div([
+                html.H1(f"Welcome, {user_info['name']}"),
+                html.P(f"Email: {user_info['email']}")
+            ])
+        return "Please login with Google."
+    else:
+        ""
+    return ""
+
+
     
 
 if __name__ == '__main__':
