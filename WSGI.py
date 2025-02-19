ac30caeb52507bd5d607dd1dc2e47425e025c334
John Ellis <145600714+john-a-ellis@users.noreply.github.com>
2025-02-19 10:35:37 -0500

    Adding WSGI.py to the repository

---
+++ WSGI.py
@@ -0,0 +1,11 @@
+from app import app
+
+# The server variable must be named 'application' for WSGI servers
+application = app.server
+
+# Some WSGI servers also look for 'server'
+server = application
+
+# # Optional: If you want to run the file directly
+# if __name__ == '__main__':
+#     app.run_server()
