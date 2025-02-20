7bda80a1a6b2771d1630ac7f18521bf6a19305e8
John Ellis <145600714+john-a-ellis@users.noreply.github.com>
2025-02-20 12:37:20 -0500

    align file with version deployed

--- WSGI.py
+++ WSGI.py
@@ -3,15 +3,10 @@ import os
 
 # Add the application directory to the Python path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, '/var/www/webroot/ROOT')
+os.chdir('/var/www/webroot/ROOT')
 
-
-from app import app
-
-# The server variable must be named 'application' for WSGI servers
-application = app.server
-
-# Some WSGI servers also look for 'server'
-server = application
+from app import server as application
 
 # # Optional: If you want to run the file directly
 if __name__ == '__main__':
